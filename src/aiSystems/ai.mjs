c17f0297cec396434ee8824fd365bd8a14dd9e4c
Psychpsyo <psychpsyo@gmail.com>
2024-05-03 17:05:58 +0200

    Add random AI

--- src/aiSystems/ai.mjs
+++ src/aiSystems/ai.mjs
@@ -1,4 +1,5 @@
 // exports the base class for any AI implementation
+
 export class AI {
 	// this should return an input response object, just like a player would.
 	async selectMove(optionList, player) {}
---
+++ src/aiSystems/randomAI.mjs
@@ -0,0 +1,26 @@
+import {AI} from "./ai.mjs";
+import * as requests from "../inputRequests.mjs";
+
+// an AI that just picks random options at any possible time.
+// NOT RECOMMENDED FOR ACTUAL GAMES because:
+// fully random Cross Universe is very unintuitive as stacks & blocks
+// tend to be used in weird ways and attacks generally make no sense
+
+export class RandomAI extends AI {
+	async selectMove(optionList, player) {
+		while (optionList.length > 0) {
+			const option = optionList.splice(Math.floor(Math.random(optionList.length)), 1)[0];
+			const responses = [];
+			for (const response of requests[option.type].generateValidResponses(option)) {
+				responses.push(response);
+			}
+			if (responses.length === 0) continue;
+			return {
+				type: option.type,
+				value: responses[Math.floor(Math.random(responses.length))]
+			};
+		}
+		// we didn't have a valid choice
+		throw new Error("Random AI received no valid option!");
+	}
+}
